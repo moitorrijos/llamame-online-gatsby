b650e9deb04804ae77295595d2d35d017bb91991
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2020-04-21 15:20:07 -0500

    Fix overflow problem with header svg. Add link to conference in video conference button.

--- src/components/video-conference.js
+++ src/components/video-conference.js
@@ -3,7 +3,7 @@ import VideoIcon from "../icons/video.svg"
 import "../styles/_video-conference.sass"
 
 const VideoConference = ({ color }) => (
-  <a href="#0" className={`conference-link ${color}`}>
+  <a href="https://hey.llamame.online/" className={`conference-link ${color}`}>
     <VideoIcon />
     Inicia una Conferencia
   </a>
