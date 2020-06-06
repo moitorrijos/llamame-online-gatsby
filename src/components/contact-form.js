9920f66381e87c05c74bfaf91ea0c470b50834f3
moitorrijos <juanmtorrijos@gmail.com>
2020-06-06 16:10:04 -0500

    Add mutation for contact form. Remove formik from contact form.

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,93 +1,132 @@
-import React from "react"
-import { useFormik } from "formik"
+import React, { useState } from "react"
+import gql from "graphql-tag"
+import { Mutation } from "react-apollo"
 import Send from "../icons/send.svg"
 import Sending from "../icons/refresh.svg"
-const ContactForm = () => {
-  async function wait(ms) {
-    return new Promise(resolve => {
-      setTimeout(resolve, ms)
-    })
-  }
-  const formik = useFormik({
-    initialValues: {
-      tuNombre: "",
-      tuCorreo: "",
-      tuTelefono: "",
-      tuMensaje: "",
-      saludos: "",
-    },
-    onSubmit: async values => {
-      try {
-        if (values.saludos !== "") {
-          console.log("Error")
-          return
-        }
-        const response = await wait(2000)
-        console.log(response)
-      } catch (error) {
-        console.error(error)
+
+const contact_mutation = gql`
+  mutation CreateSubmissionMutation(
+    $clientMutationId: String!
+    $tuNombre: String!
+    $tuCorreo: String!
+    $tuTelefono: String!
+    $tuMensaje: String!
+  ) {
+    createSubmission(
+      input: {
+        clientMutationId: $clientMutationId
+        tuNombre: $tuNombre
+        tuCorreo: $tuCorreo
+        tuTelefono: $tuTelefono
+        tuMensaje: $tuMensaje
       }
-    },
-  })
+    ) {
+      success
+      data
+    }
+  }
+`
+const ContactForm = () => {
+  const [tuNombre, setTuNombre] = useState("")
+  const [tuCorreo, setTuCorreo] = useState("")
+  const [tuTelefono, setTuTelefono] = useState("")
+  const [tuSaludo, setTuSaludo] = useState("")
+  const [tuMensaje, setTuMensaje] = useState("")
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="tuNombre">
-        Tu Nombre <small className="required">necesario</small>
-        <input
-          type="text"
-          name="tuNombre"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.tuNombre}
-          required="required"
-        />
-      </label>
-      <label htmlFor="tuCorreo">
-        Tu Correo Electrónico <small className="required">necesario</small>
-        <input
-          type="email"
-          name="tuCorreo"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.tuCorreo}
-          required="required"
-        />
-      </label>
-      <label htmlFor="tuTelefono">
-        Tu Número de Teléfono
-        <input
-          type="text"
-          name="tuTelefono"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.tuTelefono}
-        />
-      </label>
-      <label className="saludos" htmlFor="saludos">
-        Saludos
-        <input
-          type="text"
-          name="saludos"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.saludos}
-        />
-      </label>
-      <label htmlFor="tuMensaje">
-        Tu Mensaje <small className="required">necesario</small>
-        <textarea
-          name="tuMensaje"
-          required="required"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.tuMensaje}
-        />
-      </label>
-      <button type="submit" className={formik.isSubmitting ? "enviando" : ""}>
-        {formik.isSubmitting ? "Enviando" : "Enviar"}
-        {formik.isSubmitting ? <Sending /> : <Send />}
-      </button>
-    </form>
+    <Mutation mutation={contact_mutation}>
+      {(createSubmission, { loading, error, data }) => (
+        <>
+          <form
+            onSubmit={async event => {
+              event.preventDefault()
+              createSubmission({
+                variables: {
+                  clientMutationId: "prueba",
+                  tuNombre,
+                  tuCorreo,
+                  tuTelefono,
+                  tuSaludo,
+                  tuMensaje,
+                },
+              })
+            }}
+          >
+            <label htmlFor="tuNombre">
+              Tu Nombre <small className="required">necesario</small>
+              <input
+                type="text"
+                name="tuNombre"
+                onChange={event => {
+                  setTuNombre(event.target.value)
+                }}
+                value={tuNombre}
+                required="required"
+              />
+            </label>
+            <label htmlFor="tuCorreo">
+              Tu Correo Electrónico{" "}
+              <small className="required">necesario</small>
+              <input
+                type="email"
+                name="tuCorreo"
+                onChange={event => {
+                  setTuCorreo(event.target.value)
+                }}
+                value={tuCorreo}
+                required="required"
+              />
+            </label>
+            <label htmlFor="tuTelefono">
+              Tu Número de Teléfono
+              <input
+                type="text"
+                name="tuTelefono"
+                onChange={event => {
+                  setTuTelefono(event.target.value)
+                }}
+                value={tuTelefono}
+              />
+            </label>
+            <label className="saludos" htmlFor="saludos">
+              Saludos
+              <input
+                type="text"
+                name="saludos"
+                onChange={event => {
+                  setTuSaludo(event.target.value)
+                }}
+                value={tuSaludo}
+              />
+            </label>
+            <label htmlFor="tuMensaje">
+              Tu Mensaje <small className="required">necesario</small>
+              <textarea
+                name="tuMensaje"
+                required="required"
+                onChange={event => {
+                  setTuMensaje(event.target.value)
+                }}
+                value={tuMensaje}
+              />
+            </label>
+            <button type="submit" className={loading ? "enviando" : ""}>
+              {loading ? "Enviando" : "Enviar"}
+              {loading ? <Sending /> : <Send />}
+            </button>
+          </form>
+          {error && (
+            <div className="error">
+              Disculpa, ha ocurrido un error. Vuelve a intentarlo más tarde.
+            </div>
+          )}
+          {data && (
+            <div className="success">
+              Gracias por contactarnos. Te estaremos escribiendo muy pronto.
+            </div>
+          )}
+        </>
+      )}
+    </Mutation>
   )
 }
 
